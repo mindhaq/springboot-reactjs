ea7babef1d68e4a311e9b0cb3bb5f93a33c6951a
Rüdiger Schulz <rueschu@gmail.com>
2017-08-18 03:41:57 +0200

    initial app created on start.spring.io

---
+++ src/test/java/com/mindhaq/springbootreact/SpringbootReactApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mindhaq.springbootreact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootReactApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
